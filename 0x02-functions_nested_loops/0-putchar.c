a3052efca2b49d94af3a5c8c6b37537db88288ef
Ali <chrifali00@gmail.com>
2023-03-22 19:22:33 -0700

    putch

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,19 +7,16 @@
  *
  * Return: Always 0 (Success)
  */
-
-int _putchar(char c);
-
 int main(void)
 {
-    putchar('');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return (0);
+	putchar('');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
